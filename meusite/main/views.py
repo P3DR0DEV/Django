6bd3221aa6a8affab88c25b50ba77923d3b8486c
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-06-07 17:06:50 -0300

    List

--- meusite/main/views.py
+++ meusite/main/views.py
@@ -7,7 +7,7 @@ from .models import ToDoList
 def index(response, id):
     ls = ToDoList.objects.get(id= id) #Adiciona o nome da lista
     #item = ls.item_set.get(id=1) #Adiciona os itens da lista
-    return render(response, "main/list.html", {})
+    return render(response, "main/list.html", {"ls": ls})
 
 def home(response):
     return render(response, "main/home.html", {})
