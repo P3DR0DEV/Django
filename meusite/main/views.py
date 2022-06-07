bf943f22cc7523a707543a8b0e4630ddd0c16ba0
Pedro <pedrohenriquecamposmendes@hotmail.com>
2022-06-06 22:05:26 -0300

    Database

--- meusite/main/views.py
+++ meusite/main/views.py
@@ -3,8 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Teste</h1>")
+def index(response, id):
+    return HttpResponse("<h1>%s</h1>" % id)
 
-def v1(response):
-    return HttpResponse("<h1>Teste de navegação</h1>")
