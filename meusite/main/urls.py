3e1487038116facb138ef5c27668e109155e388b
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-06-10 12:09:25 -0300

    Update

--- meusite/main/urls.py
+++ meusite/main/urls.py
@@ -4,11 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # pagina index;
-    # nome da lista na URL // <int:id> id da lista na url
-    path("<int:id>", views.index, name="index"), 
-    #home page
-    path("1", views.home, name = 'home'),
-    #pagina base 
-    path("", views.base, name = 'base')
+    # ToDoList
+    # Nome da lista na URL // <int:id> id da lista na url
+    path("<int:id>", views.index, name="index"),  
+    #Home Page 
+    path("", views.home, name = 'home')
 ] 
