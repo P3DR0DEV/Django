15ccb51419ccb7194f34594db3fded0ac5eee588
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-06-13 17:37:26 -0300

    Models Update

--- meusite/register/views.py
+++ meusite/register/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
@@ -6,5 +6,9 @@ from django.contrib.auth.forms import UserCreationForm
 def register(response):
     if response.method == 'POST':
         form = UserCreationForm(response.POST)
-        
+        if form.is_valid():
+            form.save()
+        redirect('/home')
+    else:
+        form = UserCreationForm()
     return render(response, "register/register.html",{"form":form})
